daaa9ebbb88c7708356ab57453d5d66cd77d567a
thuralvesss <arthurg8g8@gmail.com>
2026-03-14 15:05:56 -0300

    feat: projeto Spring Boot base criado com Maven e H2 configurado

---
+++ maktaba-api/src/main/java/br/com/maktaba/MaktabaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.maktaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaktabaApiApplication.class, args);
+	}
+
+}
---
+++ maktaba-api/src/test/java/br/com/maktaba/MaktabaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maktaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaktabaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
