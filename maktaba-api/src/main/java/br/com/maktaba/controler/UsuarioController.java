6217e2b014fb91fb872e7d9eae711f4b6a2c5982
thuralvesss <arthurg8g8@gmail.com>
2026-03-15 19:07:02 -0300

    Finalização da Sprint 1: Telas de Login/Cadastro e rotas de Registro

--- maktaba-api/src/main/java/br/com/maktaba/controler/UsuarioController.java
+++ maktaba-api/src/main/java/br/com/maktaba/controler/UsuarioController.java
@@ -3,26 +3,31 @@ package br.com.maktaba.controler;
 import br.com.maktaba.model.Usuario;
 import br.com.maktaba.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/usuarios") // Este será o endereço oficial: localhost:8080/usuarios
+@Controller
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository repository;
 
-    // Rota para mostrar todos os usuários cadastrados
-    @GetMapping
-    public List<Usuario> listarTodos() {
-        return repository.findAll();
+    @GetMapping("/cadastro")
+    public String exibirCadastro() {
+        return "cadastro";
     }
 
-    // Rota para salvar um novo usuário no banco de dados
-    @PostMapping
-    public Usuario cadastrar(@RequestBody Usuario usuario) {
-        return repository.save(usuario);
+    @GetMapping("/login")
+    public String exibirLogin() {
+        return "login";
+    }
+
+    @PostMapping("/registrar")
+    public String registrar(Usuario usuario) {
+        repository.save(usuario);
+        return "redirect:/usuarios/login";
     }
 }
